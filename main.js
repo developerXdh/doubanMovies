8f5d27ed99b12094706ce7ad485cdb97db3ddda5
zzy <zhaozy086@163.com>
2019-04-18 20:29:42 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+$(function(){
+  $.ajax({
+    url:"https://api.douban.com/v2/movie/in_theaters",
+    data:{
+      count:50
+    },
+    dataType:"jsonp",
+    jsonp: "callback",
+    success:function(res){
+      res.subjects.forEach(element => {
+        $(".hot-parent-node").append(
+`<div class="hot col-lg-4 col-md-6">
+    <div>
+      <a href="${element.alt}"><img src="${element.images.large}"/></a>
+    </div>
+    <div>
+      <h1>${element.title}</h1>
+      <p><span class="text-danger">${element.rating.average}分</span> / ${element.collect_count}收藏</p>
+      <p>${element.year}年 / ${element.genres[0]} / ${element.genres[1]}</p>
+      <p>导演：${element.directors[0].name}</p>
+      <p>主演：${element.casts[0].name} / ${element.casts[1].name} / ${element.casts[2].name}</p>
+    </div>
+</div>`
+        )
+      })
+    },
+    error:function(err){
+      console.log(err)
+    }
+  })
+})
